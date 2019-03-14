31d8de7d300fd880ab400d362173bc8389a682b9
Peter J. Pouliot <peter@pouliot.net>
2019-03-14 17:09:56 +0000

    updating

--- ansible/files/pin_docker-ce.sh
+++ ansible/files/pin_docker-ce.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-apt-get --purge autoremove docker-ce -y && apt-get install docker-ce=18.06.1-ce -y
-apt-mark hold docker-ce
+apt-get --purge autoremove docker-ce -y && apt-get install docker.io -y
